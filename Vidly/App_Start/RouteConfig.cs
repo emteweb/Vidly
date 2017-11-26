3d6ee0ba8e245c552451f31f8abf21b1c8067f4c
Marcin Trebacz <marcintrebacz79@gmail.com>
2017-11-26 19:40:42 +0100

    Before first exercise

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,16 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // we add this line when we give a new route as an attribute in a controller
+            routes.MapMvcAttributeRoutes();
+
+            // this is the old way of adding a route 
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new {controller="Movies",action="ByReleaseDate"},
+            //    new {year=@"\d{4}", month=@"\d{2}" }
+            //    );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
